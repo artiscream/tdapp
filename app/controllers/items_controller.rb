a507d7636092af8a1c83ee2b537035a0ea0a99cc
artiscream <arti_scream@ukr.net>
2019-01-23 22:15:23 +0200

    final styling

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,11 +9,11 @@ class ItemsController < ApplicationController
 	end
 
 	def new
-		@item = current_user.item.build
+		@item = current_user.items.build
 	end
 
 	def create
-		@item = current_user.item.build(item_params)
+		@item = current_user.items.build(item_params)
 		if @item.save
 			redirect_to root_path
 		else
