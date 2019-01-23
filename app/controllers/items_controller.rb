7bf4290b80bdb5a9e08c5c88ee5a353a7e6cd0bf
artiscream <arti_scream@ukr.net>
2019-01-23 22:00:37 +0200

    add completed at ab

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-	before_action :find_item, only: [:show, :edit, :update, :destroy]
+	before_action :find_item, only: [:show, :edit, :update, :destroy, :complete]
 	before_action :authenticate_user!, except: [:index, :show]
 
 	def index
@@ -37,6 +37,11 @@ class ItemsController < ApplicationController
 
 	def destroy
 		@item.destroy
+		redirect_to root_path
+	end
+
+	def complete	
+		@item.update_attribute(:completed_at, Time.now)
 		redirect_to root_path
 	end
 
